1cfca76e1012be0bea25e19ebe19fb7a9cfbafa6
Max <max@MaxdeMacBook-Pro.local>
2015-12-16 00:25:57 +0800

    update

---
+++ MXNavigationController/MXNavigator/UIViewController+gesture.h
@@ -0,0 +1,13 @@
+//
+//  UIViewController+gesture.h
+//  MXNavigationController
+//
+//  Created by Max on 15/12/16.
+//  Copyright © 2015年 Max. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController (gesture)
+
+@end
---
+++ MXNavigationController/MXNavigator/UIViewController+gesture.m
@@ -0,0 +1,54 @@
+//
+//  UIViewController+gesture.m
+//  MXNavigationController
+//
+//  Created by Max on 15/12/16.
+//  Copyright © 2015年 Max. All rights reserved.
+//
+
+#import "UIViewController+gesture.h"
+#import "objc/runtime.h"
+
+@implementation UIViewController (gesture)
+
++ (void)load {
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        swizzleMethod([self class], @selector(viewDidLoad), @selector(swizzling_viewDidLoad));
+    });
+}
+
+void swizzleMethod(Class class, SEL originalSelector, SEL swizzledSelector)
+{
+    Method originalMethod = class_getInstanceMethod(class, originalSelector);
+    Method swizzledMethod = class_getInstanceMethod(class, swizzledSelector);
+    
+    BOOL didAddMethod =
+    class_addMethod(class,
+                    originalSelector,
+                    method_getImplementation(swizzledMethod),
+                    method_getTypeEncoding(swizzledMethod));
+    
+    if (didAddMethod) {
+        class_replaceMethod(class,
+                            swizzledSelector,
+                            method_getImplementation(originalMethod),
+                            method_getTypeEncoding(originalMethod));
+    } else {
+        method_exchangeImplementations(originalMethod, swizzledMethod);
+    }
+}
+
+- (void)swizzling_viewDidLoad {
+    UIScreenEdgePanGestureRecognizer *gesture = [[UIScreenEdgePanGestureRecognizer alloc] initWithTarget:self
+                                                                                                  action:@selector(screenEdgePanGesture:)];
+    [gesture setEdges:UIRectEdgeLeft];
+    [self.view addGestureRecognizer:gesture];
+    [self swizzling_viewDidLoad];
+
+}
+
+- (void)screenEdgePanGesture:(UIScreenEdgePanGestureRecognizer *)gesture {
+}
+
+@end
